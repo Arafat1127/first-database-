06a0333c9d607ff2da961239e438196dec79798f
KH.Arafat <mdarafatkhandakar374@gmail.com>
2025-02-22 21:34:22 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -22,14 +22,18 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         const database = client.db('Simple-Node')
-        const productCollection = database.collection('products')
-        const bookingCollection = client.db('Simple-Node').database.collection('booking')
+        const productCollection = database.collection('Products')
 
-        app.post('/add-product', (req, res) => {
+        app.get('/products', async (req, res) => {
+            const products = await productCollection.find({}).toArray();
+            res.send(products)
+        })
+
+        app.post('/add-product', async (req, res) => {
             const product = req.body;
-            product.id = productsList.length + 1
-            productsList.push(product)
-            console.log(product);
+            const result = await productCollection.insertOne(product)
+            res.send(result)
+
         })
 
 
@@ -39,35 +43,14 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send("Basic Router")
-})
-
-
-const productsList = [
-    { name: 't-shirt', price: 450, id: 1, img: "https://cdn.pixabay.com/photo/2016/12/06/09/31/blank-1886008_640.png" },
-    { name: 'shirt', price: 250, id: 2, img: "https://m.media-amazon.com/images/I/91V12ufIXRL._AC_UY1100_.jpg" },
-    { name: 'lungi', price: 150, id: 3, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStIi5nxwpFllykK36KWi37bGM_ackeZGgO7A&s" },
-    { name: 'panjabi', price: 350, id: 4, img: "https://sobkisubazar.com/public/uploads/all/99YVJMlu6OQi1WAnqyuf8IzmOPBCySQaKTzE9ZHA.jpg" },
-
-]
-
-
-app.get('/products', (req, res) => {
-    res.send(productsList)
-})
-
 //specific product search by id == route
-app.get('/products/:id', (req, res) => {
-    const id = req.params.id;
-    const searchProduct = productsList.find(product => product.id === parseInt(id)) || {}
-    res.send(searchProduct)
-    console.log(searchProduct);
+// app.get('/products/:id', (req, res) => {
+//     const id = req.params.id;
+//     const searchProduct = productsList.find(product => product.id === parseInt(id)) || {}
+//     res.send(searchProduct)
+//     console.log(searchProduct);
 
-})
-
-
+// })
 
 
 app.listen(port, () => {
